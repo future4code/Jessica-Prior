648627ecc814dc1a83cecd5ad2c4072cdc616e88
JessicaPrior <jsscprior@gmail.com>
2020-10-19 16:34:40 -0300

    Exercicios de node-js aula 1

---
+++ semana15/semana15-aula1/index.js
@@ -0,0 +1,35 @@
+//Exercicio 1
+// A- é feita uma constante que recebe process.argv´["valor que quer acessar"] e depois utiliza um consolelog com ${"variavel"} para mostrar
+
+// B-
+// const name = process.argv[2]
+// const idade = Number(process.argv[3])
+
+// console.log(`Olá ${name}! Você tem ${idade} anos`)
+
+// C-
+// const name = process.argv[2]
+// const idade = Number(process.argv[3])
+
+// console.log(`Olá, ${name}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`)
+
+//Exercicio 2
+
+// const operacao = (process.argv[2])
+// const num1 = Number(process.argv[3])
+// const num2 = Number(process.argv[4])
+
+// switch(operacao){
+//     case "soma":
+//         console.log(`A soma do numero ${num1} com ${num2} é: ${num1 + num2}`)
+//         break;
+//     case "sub":
+//         console.log(`A subtração do numero ${num1} com ${num2} é: ${num1 - num2}`)
+//         break;
+//     case "div":
+//         console.log(`A divisão do numero ${num1} com ${num2} é: ${num1 / num2}`)
+//         break;
+//     case "multi":
+//         console.log(`A soma do numero ${num1} com ${num2} é: ${num1 * num2}`)
+//         break;
+// }
