6eb7a37875de11e42074df8ca4b74714aaa077dd
JessicaPrior <jsscprior@gmail.com>
2020-11-27 17:00:44 -0300

    Projeto LAMA

---
+++ semana20/semana20-projeto/lama-template/src/index.ts
@@ -0,0 +1,21 @@
+import {AddressInfo} from "net";
+import express from "express";
+import { userRouter } from "./routes/userRouter";
+import { bandRouter } from "./routes/bandRouter";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/band", bandRouter)
+app.use("/show",)
+
+const server = app.listen(3000, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Servidor rodando em http://localhost:${address.port}`);
+    } else {
+      console.error(`Falha ao rodar o servidor.`);
+    }
+  });
